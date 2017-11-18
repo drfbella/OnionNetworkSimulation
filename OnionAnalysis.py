5b0605f2f55e38b21889d8ded7bfc3a929e2cec5
dslima <daniel.lima@mail.mcgill.ca>
2017-11-18 16:49:36 -0500

    Implemented Random Network and preliminary Dijkstra

---
+++ OnionAnalysis.py
@@ -0,0 +1,37 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+G=nx.dense_gnm_random_graph(10,20)
+
+for (u, v) in G.edges():
+    rand = random.randint(0,50)
+    print(rand)
+    G.edge[u][v]['weight'] = rand
+nx.draw_networkx(G)
+
+nx.draw_networkx(G)
+weight=nx.get_edge_attributes(G,'weight')
+print(weight)
+
+plt.draw()
+plt.show()
+
+
+def modify_djikstra(G, source, target):
+    paths = list(nx.all_simple_paths(G, source=source, target=target))
+    reliabilities = []
+    for p in paths:
+        tolRel = 1
+        for i in range(len(p) - 1):
+            tolRel = tolRel * float(G[p[i]][p[i + 1]]['r'])
+        reliabilities.append(tolRel)
+    path_sorted = []
+    reliabilities_sorted = []
+    for i in range(len(reliabilities)):
+        index, value = max(enumerate(reliabilities), key=operator.itemgetter(1))
+        path_sorted.append(paths[index])
+        reliabilities_sorted.append(value)
+        del paths[index]
+        del reliabilities[index]
+    return path_sorted, reliabilities_sorted
