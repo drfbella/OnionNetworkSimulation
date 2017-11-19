d153deaaeb0efb3a1d26db1dca571e8d9d18ae92
unknown <drifa.bellache@mail.mcgill.ca>
2017-11-18 19:42:40 -0500

    Seperated  the nodes into 3 sets, selected a node randomly from each set and performed djikstra on each node. Obtained a time for path

--- OnionAnalysis.py
+++ OnionAnalysis.py
@@ -6,12 +6,10 @@ import math
 
 """
 Rebuilds the path based on the distance and previous lists provided by Djikstra's.
-
 @param source ID of the node from which to start the pathing.
 @param target ID of the node to which our path ends.
 @param distance List of distances between nodes.
 @param previous List of ancestors.
-
 @return List representing the path.
 """
 
@@ -27,11 +25,9 @@ def rebuild_path(source, target, distance, previous):
 
 """
 Executes the Djikstra algorithm on the graph given a source and a target.
-
 @param G networkx graph object
 @param source ID of the node to start from.
 @param target ID from the node to end at.
-
 @return Lists representing distances and precedence between the two nodes selected.
 """
 
@@ -76,19 +72,55 @@ def modify_djikstra(G, source, target):
 
 
 #Generates random network, can be changed to specific networks if need be
-def Generate_Random_Network(nodes,edges,cost):
-    G=nx.dense_gnm_random_graph(nodes,edges)
-    for (u, v) in G.edges():
-        rand = random.randint(0,cost)
-        G[u][v]['weight'] = rand
-    nx.draw_networkx(G)
-    plt.draw()
-    plt.show()
+G=nx.dense_gnm_random_graph(210,450)
+for (u, v) in G.edges():
+    rand = random.randint(0,40)
+    G[u][v]['weight'] = rand
+nx.draw_networkx(G)
+plt.draw()
+plt.show()
 
+
+
+
+#G= Generate_Random_Network(210,450,40)
+
+
+N0 = random.randint(0,70)
+N1 = random.randint(71, 140)
+N2 = random.randint(141, 210)
 
 t0 = time.clock()
+distance, prev = modify_djikstra(G, 1, 92)
+path = rebuild_path(1, 92, distance, prev)
+print(path)
 t1= time.clock()
 
-rroutingTime = t1-t0
 
-Generate_Random_Network(10,20,40)
+distance, prev = modify_djikstra(G, 1, N0)
+path = rebuild_path(1, N0, distance, prev)
+print(path)
+
+distance, prev = modify_djikstra(G, N0, N1)
+path = rebuild_path(N0, N1, distance, prev)
+print(path)
+
+distance, prev = modify_djikstra(G, N1, N2)
+path = rebuild_path(N1, N2, distance, prev)
+print(path)
+
+distance, prev = modify_djikstra(G, N2, 92)
+path = rebuild_path(N2, 92, distance, prev)
+print(path)
+
+t2= time.clock()
+
+#Encryption takes 0.08ms/operation. Decryption takes 1.46ms/operation
+
+normalRouting = t1-t0
+print(normalRouting)
+onionRouting = t2-t1
+print(onionRouting)
+
+
+
