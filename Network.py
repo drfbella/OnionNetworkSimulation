877a11344cc54c4856d19c6775ae833f734f0e12
Marc Cataford <c.marcandre@gmail.com>
2017-11-20 00:02:37 -0500

    Linted.

--- Network.py
+++ Network.py
@@ -3,116 +3,105 @@ import matplotlib.pyplot as plt
 import random
 
 class Network():
-	def __init__(self, nodes, max_weight):
-		print("Network created.")
-		self.ip_pool = list()
-		self.top_tier_net = list()
-		self.mid_tier_net = list()
-		self.low_tier_net = list()
-		self.clients = list()
-		self.base_cost = 1
-		self.graph = self.generate_tiered_network(3, 3, 3, 3, 5)
-		
+  def __init__(self, capacity, weight_modifiers):
+    self.ip_pool = list()
+    self.top_tier_net = list()
+    self.mid_tier_net = list()
+    self.low_tier_net = list()
+    self.clients = list()
+    self.base_cost = 1
+    self.graph = self.generate_tiered_network(capacity["top_tier"], capacity["mid_tier"], capacity["low_tier"], capacity["clients"], weight_modifiers)
 
-	def generate_random_network(self, nodes, edges, max_weight):
-		#Generates random network, can be changed to specific networks if need be
-		G = nx.dense_gnm_random_graph(nodes, edges)
-		for (u, v) in G.edges():
-				rand = random.randint(0, max_weight)
-				G[u][v]['weight'] = rand
+  def generate_tiered_network(self, top_tier, mid_tier, low_tier, end_systems, weight_modifiers):
+    network = nx.Graph()
 
-		return G
+    for net in range(1, top_tier+1):
+      ip = self.allocate_ip(("*", 0, 0, 0))
+      self.ip_pool.append(ip)
+      self.top_tier_net.append(ip)
 
-	def generate_tiered_network(self, top_tier, mid_tier, low_tier, end_systems, max_weight):
-		network = nx.Graph()
+      network.add_node(".".join(list(map(str,ip))))
 
-		for net in range(1, top_tier+1):
-			ip = self.allocate_ip(("*", 0, 0, 0))
-			self.ip_pool.append(ip)
-			self.top_tier_net.append(ip)
+    mid_tier_net = []
 
-			network.add_node(".".join(list(map(str,ip))))
+    for net in self.top_tier_net:
+      for subnet in range(mid_tier):
+        ip = self.allocate_ip((net[0],"*",0,0))
+        self.ip_pool.append(ip)
+        self.mid_tier_net.append(ip)
 
-		mid_tier_net = []
+        network.add_node(".".join(list(map(str,ip))))
 
-		for net in self.top_tier_net:
-			for subnet in range(mid_tier):
-				ip = self.allocate_ip((net[0],"*",0,0))
-				self.ip_pool.append(ip)
-				self.mid_tier_net.append(ip)
+    self.low_tier_net = []
 
-				network.add_node(".".join(list(map(str,ip))))
+    for net in self.mid_tier_net:
+      for subnet in range(low_tier):
+        ip = self.allocate_ip((net[0],net[1],"*",0))
+        self.ip_pool.append(ip)
+        self.low_tier_net.append(ip)
 
-		self.low_tier_net = []
+        network.add_node(".".join(list(map(str,ip))))
 
-		for net in self.mid_tier_net:
-			for subnet in range(low_tier):
-				ip = self.allocate_ip((net[0],net[1],"*",0))
-				self.ip_pool.append(ip)
-				self.low_tier_net.append(ip)
+    clients = []
 
-				network.add_node(".".join(list(map(str,ip))))
+    for net in self.low_tier_net:
+      for client in range(end_systems):
+        ip = self.allocate_ip((net[0],net[1],net[2],"*"))
+        self.ip_pool.append(ip)
+        self.clients.append(ip)
 
-		clients = []
+        network.add_node(".".join(list(map(str,ip))))
 
-		for net in self.low_tier_net:
-			for client in range(end_systems):
-				ip = self.allocate_ip((net[0],net[1],net[2],"*"))
-				self.ip_pool.append(ip)
-				self.clients.append(ip)
+    #Connecting the graph
+    for net in self.top_tier_net:
+      for net2 in self.top_tier_net:
+        if net != net2:
+          network.add_edge(".".join(list(map(str,net))), ".".join(list(map(str,net2))), weight=0)
 
-				network.add_node(".".join(list(map(str,ip))))
+    for net in self.mid_tier_net:
+      network.add_edge("{}.0.0.0".format(net[0]), ".".join(list(map(str,net))), weight=0.1)
 
-		#Connecting the graph
-		for net in self.top_tier_net:
-			for net2 in self.top_tier_net:
-				if net != net2:
-					network.add_edge(".".join(list(map(str,net))), ".".join(list(map(str,net2))), weight=0)
+    for net in self.low_tier_net:
+      network.add_edge("{}.{}.0.0".format(net[0], net[1]), ".".join(list(map(str,net))), weight=0.5)
 
-		for net in self.mid_tier_net:
-			network.add_edge("{}.0.0.0".format(net[0]), ".".join(list(map(str,net))), weight=0.1)
+    for client in self.clients:
+      network.add_edge("{}.{}.{}.0".format(client[0], client[1], client[2]), ".".join(list(map(str,client))), weight=0.9)
+        
 
-		for net in self.low_tier_net:
-			network.add_edge("{}.{}.0.0".format(net[0], net[1]), ".".join(list(map(str,net))), weight=0.5)
+    return network
+    
 
-		for client in self.clients:
-			network.add_edge("{}.{}.{}.0".format(client[0], client[1], client[2]), ".".join(list(map(str,client))), weight=0.9)
-				
+  def allocate_ip(self, mask):
+    ip_candidate = list(mask)
 
-		return network
-		
+    for segment in range(len(ip_candidate)):
+      if mask[segment] == "*":
+        ip_candidate[segment] = random.randint(0, 255)
 
-	def allocate_ip(self, mask):
-		ip_candidate = list(mask)
+    ip_candidate = tuple(ip_candidate)
 
-		for segment in range(len(ip_candidate)):
-			if mask[segment] == "*":
-				ip_candidate[segment] = random.randint(0, 255)
-
-		ip_candidate = tuple(ip_candidate)
-
-		return ip_candidate if ip_candidate not in self.ip_pool else self.allocate_ip(mask)
+    return ip_candidate if ip_candidate not in self.ip_pool else self.allocate_ip(mask)
 
 
-	def draw_network(self, path):
+  def draw_network(self, path):
 
-		node_list = self.graph.nodes()
-		node_colour = list()
+    node_list = self.graph.nodes()
+    node_colour = list()
 
-		for node in node_list:
-			if node in path:
-				node_colour.append("red")
-			else:
-				node_colour.append("blue")
+    for node in node_list:
+      if node in path:
+        node_colour.append("red")
+      else:
+        node_colour.append("blue")
 
-		edge_colour = list()
+    edge_colour = list()
 
-		for edge in self.graph.edges():
-			if edge[0] in path and edge[1] in path:
-				edge_colour.append("red")
-			else:
-				edge_colour.append("black")
+    for edge in self.graph.edges():
+      if edge[0] in path and edge[1] in path:
+        edge_colour.append("red")
+      else:
+        edge_colour.append("black")
 
-		nx.draw_spring(self.graph, node_color = node_colour, edge_color = edge_colour, with_labels=True)
-		plt.draw()
-		plt.show()
+    nx.draw_spring(self.graph, node_color = node_colour, edge_color = edge_colour, with_labels=True)
+    plt.draw()
+    plt.show()
--- NetworkGenerationTest.py
+++ NetworkGenerationTest.py
@@ -1,5 +1,5 @@
 from Network import Network
 
-net = Network(0, 0)
+net = Network(0)
 
 net.draw_network()
