5acf50e80bfadaa37cea92f4ecb72034ce9341d8
Marc Cataford <c.marcandre@gmail.com>
2017-11-20 00:01:37 -0500

    Linted.

--- Message.py
+++ Message.py
@@ -1,5 +1,5 @@
 class Message():
-		def __init__(self):
-			self.source = []
-			self.destination = []
-			self.message = ""
+  def __init__(self):
+    self.source = []
+    self.destination = []
+    self.message = ""
